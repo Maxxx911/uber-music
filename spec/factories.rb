a978c4b23772fcd15e9740977ea4209d8233e064
Maxxx911 <maxrikal@gmail.com>
2019-09-12 12:55:48 +0300

    Chage factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,15 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nickname 'UserNickname'
-    firstname 'FisrtUserName'
-    second_name 'SecondUserName'
-    email 'emailUser@email.com'
-    password 'user-password'
-    confirmed_at DateTime.now
+    firstname {'FisrtUserName'}
+    second_name {'SecondUserName'}
+    email {'emailUser@email.com'}
+    password {'user-password'}
+    confirmed_at {DateTime.now}
   end
 
   factory :organization do
-    name 'Organization'
-    description 'Organization description'
+    name {'Organization'}
+    description {'Organization description'}
   end
 end
