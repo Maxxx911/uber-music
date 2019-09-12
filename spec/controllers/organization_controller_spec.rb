a39c82348105bb01e6141bc63d4b826aeecf890e
Maxxx911 <maxrikal@gmail.com>
2019-09-12 15:20:23 +0300

    add rspec for not autorization user

--- spec/controllers/organization_controller_spec.rb
+++ spec/controllers/organization_controller_spec.rb
@@ -6,14 +6,41 @@ RSpec.describe OrganizationsController do
       get :index
       expect(response).to have_http_status(200)
     end
+
     it 'render index template' do
       get :index
       expect(response).to render_template('index')
     end
+
     it 'assigns serializeration @organizations' do
       organization = create(:organization)
       get :index
       expect(assigns(:organizations)).to eq([ActiveModelSerializers::SerializableResource.new(organization).serializable_hash])
+    end
+  end
+
+  describe 'GET edit' do
+    describe 'user not autoriazation' do 
+      it 'return status 302' do
+        organization = create(:organization)
+        get :edit, params: {id: organization.id}
+        expect(response).to have_http_status(302)
+      end
+
+      it 'not render edit page' do
+        organization = create(:organization)
+        get :edit, params: {id: organization.id}
+        expect(response).to_not render_template('edit')
+      end
+    end
+    describe 'user not owner' do
+      it 'not render edit page' do
+        user = create(:user)
+        sign_in user
+        organization = create(:organization)
+        get :edit, params: {id: organization.id}
+        expect(response).to_not render_template('edit')
+      end
     end
   end
 end
